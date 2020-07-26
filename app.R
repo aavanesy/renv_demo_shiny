cb77e377aa08eb8a9758a3d54e9c508b64542f9c
aavanesy <aavanesy@outlook.com>
2020-07-26 16:41:45 +0400

    .

---
+++ app.R
@@ -0,0 +1,47 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(data.table)
+library(ggplot2)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  
+  # Application title
+  titlePanel("Test app renv"),
+  
+  # Sidebar with a slider input for number of bins 
+  # sidebarLayout(
+  #   sidebarPanel(
+  #   ),
+  # 
+  #   
+  #   # Show a plot of the generated distribution
+  #   mainPanel(
+      tableOutput('t')
+    #)
+ # )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  output$t = renderTable({
+    rbind('data table version',
+    packageDescription("data.table")$Version,
+    'ggplot version',
+    packageDescription("ggplot2")$Version
+    )
+  })
+
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ setup.R
@@ -0,0 +1,20 @@
+
+
+
+library(renv)
+#renv::init() 
+renv::snapshot() #run to lock a new change in packages
+
+renv::restore() #update packages config
+#When updated, this is the message you should see
+#* The library is already synchronized with the lockfile.
+
+#installing new packages
+options(renv.download.override = utils::download.file)
+
+#install.packages('ggplot2')
+#install.packages('Rcurl')
+installed.packages()
+
+
+#Sys.unsetenv("R_PROFILE_USER")
